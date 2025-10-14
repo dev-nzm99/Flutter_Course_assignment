55bd765e88f9b421e2b6d90b7f51302ab0b03240
nazmulislam <nazmulislam93400@gmail.com>
2025-10-14 22:14:58 +0600

    Create 10.dart

---
+++ Assignment-01/10.dart
@@ -0,0 +1,7 @@
+/*Write a Dart program to convert String to int.*/
+void main() {
+  String numberStr = "1234";
+  int number = int.parse(numberStr);
+  print("String value: $numberStr");
+  print("Integer value: $number");
+}
